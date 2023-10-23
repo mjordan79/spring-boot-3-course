2d72fe53c67468af7bd1bed499857c7b33f3eb6b
Renato Perini <renato.perini@gmail.com>
2023-10-23 22:16:11 +0200

    Adding first code for constructor based injection of beans.

---
+++ 2_spring-core/src/main/java/it/digitalnucleus/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package it.digitalnucleus.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 2_spring-core/src/main/java/it/digitalnucleus/springcoredemo/DemoController.java
@@ -0,0 +1,25 @@
+package it.digitalnucleus.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Constructor Injection Example.
+ * Generally used for mandatory dependencies.
+ */
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach coach) {
+        this.myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+
+}
